6de8b01b89fd84d98a347a25bbcf1a54b01f2491
Scassaro <stephencassaro@yahoo.com>
2014-05-07 14:58:02 -0700

    Second app.js commit
    Cleaned it up

--- Project2App.js
+++ Project2App.js
@@ -67,14 +67,12 @@ connection.query('CREATE DATABASE IF NOT EXISTS scassaro', function (err) {
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 app.set('subtitle', 'Lab 18');
-//app.set('view engine', 'ejs');
 
 // Main route sends our HTML file
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/Project2Main.html');
 });
-
 
 // Other Pages
 
@@ -85,10 +83,6 @@ app.get('/Project2EnterVehicle.html', function(req, res) {
 app.get('/Project2About.html', function(req, res) {
     res.sendfile(__dirname + '/Project2About.html');
 });
-
-// Set up Views
-
-//app.set('views',__dirname + '/views');
 
 // Update MySQL database
 
@@ -119,67 +113,63 @@ app.post('/entercomment', function(req, res) {
 		function (err, result) {
 			if (err) throw err;
 			res.send('Thank you ' + req.body.name + '! Someone will contact your shortly regarding your submission.');
-			}
-		);
+		}
+	);
 	connection.query('INSERT INTO EmailList SET name = ?', req.body.name, ',& email = ?', req.body.email,
 		function (err, result) {
 			if (err) throw err;
-			}
-		);
+		}
+	);
 });
 
 // Retrieving tables.
 
 app.get('/vehicle/table', function (req, res) {
-    connection.query('select * from Vehicle1',
-	function (err, result) {
+    connection.query('select * from Vehicle1', function (err, result) {
 	    if(result.length > 0) {
-		var responseHTML = '<html><head><title>All Vehicles</title><link a href="/default.css" rel="stylesheet"></head><body>';
-		responseHTML += '<div class="title">All Vehicles</div>';
-		responseHTML += '<table class="vehicles"><tr><th class="rightalign">Name</th><th>Vehicle Make</th><th>Vehicle Model</th><th>Vehicle Year</th><th>Value</th></tr>';
-		for(var i = 0; result.length > i; i++) {
-		    responseHTML += '<tr>' +
-			'<td><a href="/users/?id=' + result[i].id + '"></a>' + result[i].name + '</td>' + 
-			'<td>' + result[i].make + '</td>' + 
-			'<td>' + result[i].model + '</td>' + 
-			'<td>' + result[i].year + '</td>' + 
-			'<td>' + result[i].value + '</td>' +
-			'</tr>';
-		}
-		responseHTML += '</table>';
-		responseHTML += '</body></html>';
-		res.send(responseHTML);
+			var responseHTML = '<html><head><title>All Vehicles</title><link a href="/default.css" rel="stylesheet"></head><body>';
+			responseHTML += '<div class="title">All Vehicles</div>';
+			responseHTML += '<table class="vehicles"><tr><th class="rightalign">Name</th><th>Vehicle Make</th><th>Vehicle Model</th><th>Vehicle Year</th><th>Value</th></tr>';
+			for(var i = 0; result.length > i; i++) {
+				responseHTML += '<tr>' +
+				'<td><a href="/users/?id=' + result[i].id + '"></a>' + result[i].name + '</td>' + 
+				'<td>' + result[i].make + '</td>' + 
+				'<td>' + result[i].model + '</td>' + 
+				'<td>' + result[i].year + '</td>' + 
+				'<td>' + result[i].value + '</td>' +
+				'</tr>';
+			}
+			responseHTML += '</table>';
+			responseHTML += '</body></html>';
+			res.send(responseHTML);
 	    }
 	    else
-		res.send('No users exist.');
-	}
-		    );
+			res.send('No users exist.');
+	});
 });
 
 app.get('/review/table', function (req, res) {
-    connection.query('select * from Reviews',
-	function (err, result) {
+    connection.query('select * from Reviews', function (err, result) {
 	    if(result.length > 0) {
-		var responseHTML = '<html><head><title>All Reviews</title><link a href="/default.css" rel="stylesheet"></head><body>';
-		responseHTML += '<div class="title">All Reviews</div>';
-		responseHTML += '<table class="vehicles"><tr><th class="rightalign">Name</th><th>Vehicle Make</th><th>Vehicle Model</th><th>Vehicle Year</th><th>Review</th></tr>';
-		for(var i = 0; result.length > i; i++) {
-		    responseHTML += '<tr>' +
-			'<td><a href="/users/?id=' + result[i].id + '"></a>' + result[i].name + '</td>' + 
-			'<td>' + result[i].make + '</td>' + 
-			'<td>' + result[i].model + '</td>' + 
-			'<td>' + result[i].year + '</td>' + 
-			'<td>' + result[i].review + '</td>' +
-			'</tr>';
-		}
-		responseHTML += '</table>';
-		responseHTML += '</body></html>';
-		res.send(responseHTML);
+			var responseHTML = '<html><head><title>All Reviews</title><link a href="/default.css" rel="stylesheet"></head><body>';
+			responseHTML += '<div class="title">All Reviews</div>';
+			responseHTML += '<table class="vehicles"><tr><th class="rightalign">Name</th><th>Vehicle Make</th><th>Vehicle Model</th><th>Vehicle Year</th><th>Review</th></tr>';
+			for(var i = 0; result.length > i; i++) {
+				responseHTML += '<tr>' +
+				'<td><a href="/users/?id=' + result[i].id + '"></a>' + result[i].name + '</td>' + 
+				'<td>' + result[i].make + '</td>' + 
+				'<td>' + result[i].model + '</td>' + 
+				'<td>' + result[i].year + '</td>' + 
+				'<td>' + result[i].review + '</td>' +
+				'</tr>';
+			}
+			responseHTML += '</table>';
+			responseHTML += '</body></html>';
+			res.send(responseHTML);
 	    }
 	    else
-		res.send('No users exist.');
-	}
-		    );
+			res.send('No users exist.');
+	});
 });
 
 // Populate Select
@@ -190,9 +180,9 @@ app.post('/vehicle/select', function (req, res) {
 	    console.log(result);
 	    var responseHTML = '<select id="vehicle-list">';
 	    for (var i=0; result.length > i; i++) {
-		var option = '<option value="' + result[i].id + '">' + result[i].name + '</option>';
- 		console.log(option);
-		responseHTML += option;
+			var option = '<option value="' + result[i].id + '">' + result[i].name + '</option>';
+			console.log(option);
+			responseHTML += option;
 	    }
 	    responseHTML += '</select>';
 	    res.send(responseHTML);
@@ -202,7 +192,6 @@ app.post('/vehicle/select', function (req, res) {
 // Return info for select
 app.get('/vehicle/', function (req, res) {
     console.log(req.body);
-    //if(typeof req.query.id != 'undefined') {
 	connection.query('select * from Vehicle1 where id = ?', req.query.id,
 			 function (err, result) {
 			     console.log(result);
@@ -222,48 +211,8 @@ app.get('/vehicle/', function (req, res) {
 			     else
 				 res.send('User does not exist.');
 			 }
-			)/*}*/;
-    /*else if(typeof req.query.username != 'undefined') {
-	connection.query('select * from Vehicle where name = ?', req.query.name,
-		function (err, result) {
-		    console.log(result);
-		    if(result.length > 0) {
-			res.send('Username: ' +*/
+			);
 });
-    
-
-// Stuff for Lab 18 VIEWS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-/*app.get('/users', function(req, res) {
-    var result = [
-	{UserID: 1, Email: 'Test'},
-	{UserID: 2, Email: 'Test2'}
-];
-    res.render('displayUserTable.ejs', {rs: result});
-});
-
-
-app.get('/first',  function(req, res) {
-    connection.query('select model from Vehicle1 where make="Chevy"',
-		     function (err, result){
-			 if(result.length > 0)
-			     var content = [
-				 {Model: result.model}
-				 ];
-			 res.render('lab18first.ejs', {rs: content});
-		     });
-});
-
-app.get('/second',  function(req, res) {
-    res.render('lab18second.ejs')
-});*/
-
-// Use app.js to view a file (uSING VIEWS)
-
-app.get('/lab18', function(req, res) {
-    res.render('lab18');
-    }
-);
 
 // Begin listening
 
